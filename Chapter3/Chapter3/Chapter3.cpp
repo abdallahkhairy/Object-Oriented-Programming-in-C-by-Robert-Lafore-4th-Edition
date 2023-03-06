b95a509034702a65ac995d183f0c033f46574a0c
khairy <abdullahkhairy98@gmail.com>
2023-03-06 20:01:18 +0200

    testing

--- Chapter3/Chapter3/Chapter3.cpp
+++ Chapter3/Chapter3/Chapter3.cpp
@@ -36,23 +36,17 @@ int main()
 // displays prime number distribution
 
  
-        const unsigned char WHITE = 219; //solid color (primes)
-        const unsigned char GRAY = 176; //gray (non primes)
-        unsigned char ch;
-        //for each screen position
-        for (int count = 0; count < 80 * 25 - 1; count++)
+   //ex 1
+    int number = 1;
+    cout << "Enter number" << endl;
+    cin >> number;
+    for (int i = 1; i <= 20; i++)
+    {
+        for (int j = 1; j <= 10; j++)
         {
-            ch = WHITE; //assume it’s prime
-                for (int j = 2; j < count; j++) //divide by every integer from
-                    if (count % j == 0) //2 on up; if remainder is 0,
-                    {
-                        ch = GRAY; //it’s not prime
-                        break; //break out of inner loop
-                    }
-            cout << ch; //display the character
+            cout << number * j << "\t";
         }
-        //freeze screen until keypress
-        return 0;
+    }
  
 }
 
